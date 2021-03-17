003c67d1a0d640c15181cc2c627e3f16765c9cd5
shaobaitao <noob@shaobaitao.cn>
2021-03-17 21:25:46 +0800

    phpForum 1.10

--- getUserLoginStatus.php
+++ getUserLoginStatus.php
@@ -2,9 +2,9 @@
 require 'component/MysqlConnection.php';
 session_start();
 
-if(isset($_SESSION['username'])){
-    $msg=$_SESSION['username'];
-    $is_login=true;
+if (isset($_SESSION['username'])) {
+    $msg = $_SESSION['username'];
+    $is_login = true;
 
     $conn = new Connection();
 
@@ -15,17 +15,26 @@ if(isset($_SESSION['username'])){
     $stmt->execute();
     $result = $stmt->get_result();
     $row = $result->fetch_assoc();
-    $id=$row['id'];
-}else{
-    $msg='no sessions';
-    $is_login=false;
+    $id = $row['id'];
+
+    $sql = "select headPortrait from forum.forum_userInfo where userID= ? ";
+    $stmt = $conn->mysqli->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $avatar = $row['headPortrait'];
+} else {
+    $msg = 'no sessions';
+    $is_login = false;
 }
 
 $str = array
 (
-    'id'=>$id,
-    'msg'=>$msg,
-    'is_login'=>$is_login,
+    'id' => $id,
+    'msg' => $msg,
+    'is_login' => $is_login,
+    'avatar' => $avatar,
 //    'last_login'=>$username,
 );
-echo json_encode($str,256);
+echo json_encode($str, 256);
--- uploadAvatar.php
+++ uploadAvatar.php
@@ -6,38 +6,34 @@ header('Access-Control-Allow-Origin:*');
 header("Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept");
 
 session_start();
-//if(!isset($_SESSION['username'])){
-//    exit();
-//}
-echo $_SESSION['username'];
-$userID=getID($_SESSION['username']);
+if(!isset($_SESSION['username'])){
+    exit();
+}
+//echo $_SESSION['username'];
+$userID = getID($_SESSION['username']);
 
-$allowedExts = array("gif", "jpeg", "jpg", "png");
+$allowedExts = array("gif", "jpeg", "jpg", "png", "webp");
 $temp = explode(".", $_FILES["file"]["name"]);
 
 $extension = end($temp);     // 获取文件后缀名
 if ((($_FILES["file"]["type"] == "image/gif")
         || ($_FILES["file"]["type"] == "image/jpeg")
         || ($_FILES["file"]["type"] == "image/jpg")
-        || ($_FILES["file"]["type"] == "image/png"))
-    && ($_FILES["file"]["size"] < 204800)   // 小于 200 kb
+        || ($_FILES["file"]["type"] == "image/png")
+        || ($_FILES["file"]["type"] == "image/webp"))
+    && ($_FILES["file"]["size"] <= 512000)   // 小于等于 500 kb
     && in_array($extension, $allowedExts)) {
     if ($_FILES["file"]["error"] > 0) {
-        statusCode(600,"错误：: " . $_FILES["file"]["error"]);
+        statusCode(600, "错误：: " . $_FILES["file"]["error"]);
     } else {
         // 判断当前目录下的 upload 目录是否存在该文件
         // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
-        $_FILES["file"]["name"]=uniqid().'.'.$extension;
+        $_FILES["file"]["name"] = uniqid() . '.' . $extension;
         if (file_exists("avatar/" . $_FILES["file"]["name"])) {
-            $_FILES["file"]["name"]=uniqid('sbt').'.'.$extension;
+            $_FILES["file"]["name"] = uniqid('sbt') . '.' . $extension;
         }
-        // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
         move_uploaded_file($_FILES["file"]["tmp_name"], "avatar/" . $_FILES["file"]["name"]);
+        uploadAvatar($userID, "http//:shaobaitao.cn/forumAPI/avatar/" . $_FILES["file"]["name"]);
 
-        uploadAvatar($userID,"http//:shaobaitao.cn/forumAPI/avatar/".$_FILES["file"]["name"]);
-//        echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
-//        echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
-//        echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-//        echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"] . "<br>";
     }
 }
